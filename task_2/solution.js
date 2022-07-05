3a54339f695d52b1c2f58bd1cadd3b4124c2e587
Valery Atamanov <atamva@list.ru>
2022-07-05 16:32:17 +0300

    Task 2.1 test Shiping 222

--- task_2/solution.js
+++ task_2/solution.js
@@ -6,23 +6,25 @@ function calcShipping(sum, min, shipping) {
     // Задание №2.1. Рассчитать доставку
 
     // создайте переменную shippingSum
-    let shippingSum
+    let shippingSum;
     // если productsSum равно 0,
     // то shippingSum присвоить значение 0
     if (productsSum == 0) {
-        shippingSum = 0
+        shippingSum = 0;
     }
 
     // если productsSum Больше или равна freeShippingMinSum,
     // то shippingSum присвоить значение 0
     if (productsSum >= freeShippingMinSum) {
-        shippingSum = 0
+        shippingSum = 0;
     }
     // если productsSum больше 0 и меньше freeShippingMinSum,
     // то shippingSum присвоить значение shippingPrice
     if ( 0 < productsSum < freeShippingMinSum) {
-        shippingSum = shippingPrice
+        shippingSum = shippingPrice;
     }
+
+    shippingSum = 222;
     // Конец решения задания №2.1.
 
     return shippingSum;
